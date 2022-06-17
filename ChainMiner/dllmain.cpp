c9a7e5177dd283884810eb50de58e91344d8dcff
HJH201314 <1979396625@qq.com>
2022-06-18 01:27:59 +0800

    Change lib folder.

--- ChainMiner/dllmain.cpp
+++ ChainMiner/dllmain.cpp
@@ -2,10 +2,10 @@
 #include "pch.h"
 #include "LLAPI.h"
 #include "Version.h"
-#pragma comment(lib, "../../SDK/Lib/bedrock_server_api.lib")
-#pragma comment(lib, "../../SDK/Lib/bedrock_server_var.lib")
-#pragma comment(lib, "../../SDK/Lib/SymDBHelper.lib")
-#pragma comment(lib, "../../SDK/Lib/LiteLoader.lib")
+#pragma comment(lib, "../SDK/Lib/bedrock_server_api.lib")
+#pragma comment(lib, "../SDK/Lib/bedrock_server_var.lib")
+#pragma comment(lib, "../SDK/Lib/SymDBHelper.lib")
+#pragma comment(lib, "../SDK/Lib/LiteLoader.lib")
 
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
