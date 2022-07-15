92872f66614d79d06f4e8a794d46b35e194e030a
HJH201314 <1979396625@qq.com>
2022-07-15 16:40:16 +0800

    Squashed 'SDK/' changes from 59673da0..76b81ac9
    76b81ac9 From LiteLoader 2.4.0-1-gb73d60fa1
    009a8c75 From LiteLoader 2.4.0
    b382da05 From LiteLoader 2.3.2-94-gadc3dbed3
    9d71062f From LiteLoader 2.3.2
    45fdc08b From LiteLoader 2.3.2
    e741c35d From LiteLoader 2.3.1-37-g94df7a361
    1ba63d65 From LiteLoader 2.3.1
    7e782da6 From LiteLoader 2.3.1
    3a007382 From LiteLoader 2.3.0-68-g361386827
    
    git-subtree-dir: SDK
    git-subtree-split: 76b81ac91533bf20fe26ee46a3eb39a712562e4d

--- Header/MC/Core.hpp
+++ Header/MC/Core.hpp
@@ -48,13 +48,7 @@ public:
     FileOpenMode(FileOpenMode const&) = delete;
     FileOpenMode(FileOpenMode const&&) = delete;
 };
-template <typename T0>
-class PathBuffer {
-public:
-    PathBuffer() = delete;
-    PathBuffer(PathBuffer const&) = delete;
-    PathBuffer(PathBuffer const&&) = delete;
-};
+
 struct LevelStorageResult {
     LevelStorageResult() = delete;
     LevelStorageResult(LevelStorageResult const&) = delete;
@@ -78,12 +72,40 @@ public:
     SingleThreadedLock(SingleThreadedLock const&) = delete;
     SingleThreadedLock(SingleThreadedLock const&&) = delete;
 };
-class Path {
+class PathPart
+{
+public:
+    std::string data;
+};
+
+class Path : public PathPart
+{
 public:
     Path() = delete;
     Path(Path const&) = delete;
     Path(Path const&&) = delete;
 };
+
+template <typename T>
+class PathBuffer
+{
+public:
+    PathBuffer() = delete;
+    PathBuffer(PathBuffer const&) = delete;
+    PathBuffer(PathBuffer const&&) = delete;
+	
+    T value;
+    operator T&() noexcept
+    {
+        return value;
+    }
+    operator T const&() const noexcept
+    {
+        return value;
+    }
+};
+
+
 class FilePathManager {
 public:
     FilePathManager() = delete;
@@ -103,6 +125,9 @@ Profile(Profile const&&) = delete;
 enum FileAccessType;
 
 #undef AFTER_EXTRA
+    MCAPI extern char const * RESULT_FAILED_MESSAGE;
+    MCAPI extern char const * RESULT_NOT_IMPLEMENTED_MESSAGE;
+    MCAPI extern char const * RESULT_SUCCEEDED_MESSAGE;
     MCAPI unsigned __int64 operator&(enum Core::DirectoryIterationFlags, enum Core::DirectoryIterationFlags);
     MCAPI enum Core::DirectoryIterationFlags operator|(enum Core::DirectoryIterationFlags, enum Core::DirectoryIterationFlags);
     MCAPI extern class std::shared_ptr<class Core::FileStorageArea> sRootStorageArea;
