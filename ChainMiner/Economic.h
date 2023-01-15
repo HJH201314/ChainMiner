211b72a483acc0b9b1db83227805eb69b2000e99
HJH201314 <1979396625@qq.com>
2023-01-16 01:32:21 +0800

    chore: 移除多余定义

--- ChainMiner/Economic.h
+++ ChainMiner/Economic.h
@@ -14,16 +14,9 @@ public:
     LLMoneyAPI llmoney;
     bool init();
 
-    money_t getMoney(xuid_t player);
-    bool setMoney(xuid_t player, money_t money);
-    bool addMoney(xuid_t player, money_t money);
-    bool reduceMoney(xuid_t player, money_t money);
-    bool transMoney(xuid_t player1, xuid_t player2, money_t money, string const& notes);
-    std::string getMoneyHist(xuid_t player, int time);
-    bool clearMoneyHist(int time);
-
     money_t getMoney(Player* pl);
     bool setMoney(Player* pl, money_t money);
     bool addMoney(Player* pl, money_t money);
     bool reduceMoney(Player* pl, money_t money);
+    bool clearMoneyHist(int time);
 };
