ca4315c3c2dbd622df3a241ae71e5fd23f8b1dbb
HJH201314 <1979396625@qq.com>
2023-03-13 21:28:53 +0800

    feat: Adapt to LL-2.11.x & BDS-1.19.63

--- SDK/include/llapi/mc/WeakEntityRef.hpp
+++ SDK/include/llapi/mc/WeakEntityRef.hpp
@@ -18,7 +18,10 @@ class WeakEntityRef {
 
 #define AFTER_EXTRA
 // Add Member There
-    char filler[32];
+public:
+    char filler[24];
+    LIAPI class Actor* tryUnwrap();
+
 #undef AFTER_EXTRA
 #ifndef DISABLE_CONSTRUCTOR_PREVENTION_WEAKENTITYREF
 public:
@@ -29,27 +32,22 @@ public:
 
 public:
     /**
-     * @hash   1588471742
      * @symbol  ??0WeakEntityRef\@\@QEAA\@V?$WeakRefT\@UEntityRefTraits\@\@\@\@\@Z
      */
     MCAPI WeakEntityRef(class WeakRefT<struct EntityRefTraits>);
     /**
-     * @hash   -1385648892
      * @symbol  ??4WeakEntityRef\@\@QEAAAEAV0\@$$QEAV0\@\@Z
      */
     MCAPI class WeakEntityRef & operator=(class WeakEntityRef &&);
     /**
-     * @hash   -1478353300
      * @symbol  ??8WeakEntityRef\@\@QEBA_NV?$WeakRefT\@UEntityRefTraits\@\@\@\@\@Z
      */
     MCAPI bool operator==(class WeakRefT<struct EntityRefTraits>) const;
     /**
-     * @hash   -900635137
      * @symbol  ??8WeakEntityRef\@\@QEBA_NV0\@\@Z
      */
     MCAPI bool operator==(class WeakEntityRef) const;
     /**
-     * @hash   -331487308
      * @symbol  ??1WeakEntityRef\@\@QEAA\@XZ
      */
     MCAPI ~WeakEntityRef();
